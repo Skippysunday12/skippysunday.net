30c63b6fec91fb0dc4e5f4195fc25ea1ae53d9f2
Skippysunday <skippysunday@gmail.com>
2021-12-24 15:38:49 -0600

    Began redesign, recolored icon according to new color scheme

--- index.php
+++ index.php
@@ -2,19 +2,12 @@
 <html lang="en-US" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="images/icon.png">
+    <link rel="stylesheet" href="styles/master.css">
     <title>Skippysunday</title>
-    <link rel="icon" type="image" href="images/icon.png">
-    <link rel="stylesheet" href="styles/index.css">
-    <link rel="stylesheet" href="styles/fades.css">
-    <script type="text/javascript" src="picture.js"></script>
-    <script type="text/javascript" src="headerfooter.js"></script>
   </head>
   <body>
-    <main-header/>
-    <circle-picture class="fadein" id="portrait"/>
-    <h1 class="fadein" id="welcome">Welcome!</h1>
-    <h1 class="fadein" id="welcome2">I'm Skippysunday</h1>
-    <p class="fadein" id="welcome3">I'm a small developer who makes projects for fun!</p>
+    <p id="test">Almost before we knew it</strong></p>
   </body>
 </html>
