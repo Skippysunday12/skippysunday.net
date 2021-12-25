aca2730cf5a04908328b95f360b4c6905aa13a3f
Skippysunday <skippysunday@gmail.com>
2021-12-24 23:37:09 -0600

    Adjusted color scheme and created new icon

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" href="images/icon.png">
+    <link rel="icon" href="images/favicon.png">
     <link rel="stylesheet" href="styles/master.css">
     <title>Skippysunday</title>
   </head>
   <body>
-    <p id="test">Almost before we knew it</strong></p>
+    <p>Hello. My name is Skippysunday</p>
   </body>
 </html>
