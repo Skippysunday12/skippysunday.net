a7a873ba646c66f516d858f17aa59aea919e6af7
Skippysunday <skippysunday@gmail.com>
2021-12-26 23:40:26 -0600

    Finished up main page, with a black-white gradient and fade-on-hover for project and about links

--- index.php
+++ index.php
@@ -5,9 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" href="images/icon.png">
     <link rel="stylesheet" href="styles/master.css">
+    <link rel="stylesheet" href="styles/index.css">
     <title>Skippysunday</title>
   </head>
   <body>
-    <img src="images/icon.png" alt="Profile Picture" width="600" height="500">
+    <img src="images/icon.png" alt="Profile Picture" width="600" height="500" id="profile">
+    <h1 id="welcome">Welcome!<br>I'm Skippysunday</h1>
+    <h3 id="desc">I'm a small developer who likes to work on projects in his spare time.</h3>
+    <div class="about">
+      <a href="about.php" id="aboutlink"><img src="images/about.png" alt="About" width="48" height="48" id="aboutimage">About</a>
+    </div>
+    <div class="projects">
+      <a href="projects.php" id="projectslink"><img src="images/code.png" alt="Projects" width="48" height="48" id="projectsimage">Projects</a>
+    </div>
   </body>
 </html>
